50bf1f18a1dea499b23574f19739d9816fa88d35
nbariana <nbariana@my.bcit.ca>
2024-10-31 14:30:18 -0700

    Update problem.js

--- problem.js
+++ problem.js
@@ -16,17 +16,13 @@ CHALLENGE
 function passwordCheck(password, Armida){
     if (password.input >= Armida) {
     console.log(“Access Granted!”)
+    else {
+        console.log(“Access Denied!”)
+        }
     } 
     if (password.input >= forgot) {
     console.log(“Here is a hint”)
     }
     if (password.input >= reset) {
         console.log(“Let's reset your account”)
-    }
-    
-       
-    else {
-    console.log(“Access Denied!”)
-    }
-   }
-   
+        }
